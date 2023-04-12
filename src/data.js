b40067042216487504007851da49b1171d55a851
rafifandy <rafifmochammad@gmail.com>
2023-04-12 14:56:40 +0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+    {
+        title: "Web Company Profile",
+        subtitle: "Laravel",
+        description:
+            "Web company profile with\n admin menu that used to \nmodify the web content.",
+        image: "./pal.gif",
+        link: "",
+    },
+    {
+        title: "Sales Information System",
+        subtitle: "Laravel",
+        description:
+            "This web application is designed to make it easier for store owner to manage transaction, product, stock, report, etc.",
+        image: "./cis.png",
+        link: "https://github.com/rafifandy/cis",
+    },
+    {
+        title: "Simple API",
+        subtitle: "Spring Boot",
+        description:
+            "Simple Rest API\n using Spring Boot",
+        image: "./sb.png",
+        link: "https://github.com/rafifandy/springboot-simple-api",
+    },
+    {
+        title: "Mobile Apps UI",
+        subtitle: "Figma",
+        description:
+            "An emergency call mobile apps UI\n design using Figma",
+        image: "./sos.png",
+        link: "https://www.figma.com/proto/V2JauV4YBdDC8rRIvrO7nK/FlagSOS_004_019?node-id=0%3A1",
+    },
+    {
+        title: "IoT Manual Input Testing",
+        subtitle: "Python",
+        description:
+          "Iot device test app with\n excel manual input using Python",
+        image: "./me.gif",
+        link: "",
+    },
+    {
+        title: "Dashboard Page Slicing",
+        subtitle: "React",
+        description:
+            "Slicing design from Figma\n using React",
+        image: "./pdam.gif",
+        link: "",
+    },
+  ];
+
+export const skills = ["Java","PHP","Python","JavaScript","Spring Boot", "Laravel","MySQL","PostgreSQL","HTML & CSS","React","Git","Figma UI/UX"]
