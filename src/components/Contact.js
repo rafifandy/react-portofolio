82ed502044fa580368d666f5c662555a999f0690
rafifandy <rafifmochammad@gmail.com>
2023-04-12 15:01:33 +0700

    contact edit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -77,7 +77,7 @@ export default function Contact() {
                 Contact Me
               </h2>
               <p className="leading-relaxed mb-5">
-                
+                (under development)
               </p>
               <div className="relative mb-4">
                 <label htmlFor="name" className="leading-7 text-sm text-gray-400">
