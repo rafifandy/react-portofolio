42e6fefebaaa15cfaf1bbd928a4a4cc9f9bf232b
rafifandy <64132132+rafifandy@users.noreply.github.com>
2024-11-01 14:57:38 +0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -28,11 +28,7 @@ export default function About() {
             </div>
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            // <img
-            //     className="object-cover object-center rounded"
-            //     alt="hero"
-            //     src="./pp.png"
-            // />
+            
             </div>
         </div>
     </section>
